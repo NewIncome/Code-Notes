67a2a8562ad86cc25adf8162f7b737d53e58ecb1
Alfredo C <alfredo_cardenas@live.com>
2021-08-27 16:06:12 -0500

    Add small cheatsheet info for time complexity of 'sorts' and for other algorithms

---
+++ DS&Algorithms/TimeComplexity_Cheatsheet&examples.js
@@ -0,0 +1,51 @@
+// Time Complexity(TCx) examples:
+/* ALGORITHM      Worst TCx
+  Selection Sort   O(n²)
+  Bubble Sort      O(n²)
+  Insertion Sort   O(n²)
+  Heap Sort        O(n log(n))                                (5)
+  Quick Sort       O(n²)
+  Merge Sort       O(n log(n))
+  Bucket Sort      O(n²)
+  Radix Sort       O(nk)                                      (6)
+*/
+/* LOOPS & SEARCHes
+    Worst TCx         ALGORITHMS
+   O(log2 n), O(1)  Binary Search / no loops / direct formula (1)
+      O(n)          Linear Search / 1 loop                    (2)
+    O(n log2 n)     Merge Sort    / Building segment tree     (3)
+      O(n²)         B-S-I Sort*   / 2 nested loops            (4)
+    O(n² log2 n)    2 nested loops + a tree-related DS
+      O(n³)         Floyd Warshall's
+      O(n⁴)         4 nested Loops
+    O(2^n x n)      DP with bitmask technique
+    O(2^n x n2)     DP TSP
+    O(n!), O(n⁶)    Subset, permutations
+  *B-S-I = Bubble-Selection-Intertion Sort
+*/
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (1) BINARY SEARCH
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (2) LINEAR SEARCH
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (3) MERGE SORT
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (3) BUILDING TREE STATEMENT
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (4) BUBBLE SORT
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (4) SELECTION SORT
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (4) INSERTION SORT
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (5) HEAP SORT
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (6) RADIX SORT
